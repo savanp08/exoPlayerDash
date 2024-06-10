49061ba49baf6b17ca564923e12da1142d1bf759
savanp08 <savanpoduturi@gmail.com>
2024-06-10 16:32:24 -0500

    tested with class

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,10 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,13 +12,13 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
+        jcenter() // If needed, though it's deprecated
     }
 }
-
 rootProject.name = "exoplugin"
 include(":app")
 include(":exopluginlib")
